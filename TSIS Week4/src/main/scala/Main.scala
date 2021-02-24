65f524d7a462a85f5c8a25fb08774014a3387c79
dokerbolov <proface19@gmail.com>
2021-02-24 16:50:36 +0600

    get and set

--- TSIS Week4/src/main/scala/Main.scala
+++ TSIS Week4/src/main/scala/Main.scala
@@ -3,25 +3,37 @@ import scala.io.StdIn.readLine
 object Main extends App{
 
   class Variables{
-    var num1 : Double = 0.0
-    var name: String = ""
+    private var num1 : Double = 0.0
+    private var num2: String = ""
+    def setNum1(x : Double){
+      num1 = x;
+    }
+    def getNum1(): Double ={
+      num1
+    }
+    def setNum2(x : String){
+      num2 = x;
+    }
+    def getNum2(): String ={
+      num2
+    }
   }
 
   var newVar: Variables = new Variables()
   print("Enter number: ")
-  newVar.num1 = readLine().toDouble
+  newVar.setNum1(readLine().toDouble)
   print("Enter expression: ")
-  newVar.name = readLine()
-  var compute: Double = newVar.num1
-  while(newVar.name != "="){
-    newVar.name match {
-      case "+" => print("Enter number: "); newVar.name = readLine() ; compute = compute + newVar.name.toDouble
-      case "-" => print("Enter number: "); newVar.name = readLine() ; compute = compute - newVar.name.toDouble
-      case "/" => print("Enter number: "); newVar.name = readLine() ; compute = compute / newVar.name.toDouble
-      case "*" => print("Enter number: "); newVar.name = readLine() ; compute = compute * newVar.name.toDouble
+  newVar.setNum2(readLine())
+  var compute: Double = newVar.getNum1()
+  while(newVar.getNum2() != "="){
+    newVar.getNum2() match {
+      case "+" => print("Enter number: "); newVar.setNum2(readLine()) ; compute = compute + newVar.getNum2().toDouble
+      case "-" => print("Enter number: "); newVar.setNum2(readLine()) ; compute = compute - newVar.getNum2().toDouble
+      case "/" => print("Enter number: "); newVar.setNum2(readLine()) ; compute = compute / newVar.getNum2().toDouble
+      case "*" => print("Enter number: "); newVar.setNum2(readLine()) ; compute = compute * newVar.getNum2().toDouble
     }
     print("Enter expression: ")
-    newVar.name = readLine()
+    newVar.setNum2(readLine())
   }
   println(compute)
 }
