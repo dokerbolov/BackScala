d918d6ccd9926fd1c20fe21547982cfb9885d1fa
dokerbolov <proface19@gmail.com>
2021-02-25 20:57:04 +0600

    3

--- TSIS Week5/src/main/scala/Main.scala
+++ TSIS Week5/src/main/scala/Main.scala
@@ -1,3 +1,6 @@
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 object Main extends App{
   //1
     def maxProduct(nums: Array[Int]): Int = {
@@ -9,7 +12,27 @@ object Main extends App{
       (salary.sum - salary.min - salary.max) / (salary.length - 2).toDouble
     }
   //3
-    def dayOfTheWeek(day: Int, month: Int, year: Int): String = {
-
+  def dayOfTheWeek(day: Int, month: Int, year: Int): String = {
+    var df = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+    var text = ""
+    if(month < 10){
+      if(day < 10){
+        text = "0" + day.toString + "/0" + month.toString + "/" + year.toString
+      }
+      else{
+        text = day.toString + "/0" + month.toString + "/" + year.toString
+      }
     }
+    else{
+      if(day < 10){
+        text = "0" + day.toString + "/" + month.toString + "/" + year.toString
+      }
+      else{
+        text = day.toString + "/" + month.toString + "/" + year.toString
+      }
+    }
+    var dayOfWeek = LocalDate.parse(text,df).getDayOfWeek
+    dayOfWeek.toString.toLowerCase().capitalize
+  }
+  //4
 }
