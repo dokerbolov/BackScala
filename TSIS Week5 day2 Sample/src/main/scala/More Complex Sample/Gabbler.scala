fe9cc6b5010a3924318f8309cf3d8539dcf9b534
dokerbolov <proface19@gmail.com>
2021-03-10 23:17:39 +0600

    Week5 day2

---
+++ TSIS Week5 day2 Sample/src/main/scala/More Complex Sample/Gabbler.scala
@@ -0,0 +1,24 @@
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.LoggerOps
+import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
+
+object Gabbler {
+  import ChatRoom._
+
+  def apply(): Behavior[SessionEvent] =
+    Behaviors.setup { context =>
+      Behaviors.receiveMessage {
+
+        case SessionDenied(reason) =>
+          context.log.info("cannot start chat room session: {}", reason)
+          Behaviors.stopped
+
+        case SessionGranted(handle) =>
+          handle ! PostMessage("Hello World!")
+          Behaviors.same
+        case MessagePosted(screenName, message) =>
+          context.log.info2("message has been posted by '{}': {}", screenName, message)
+          Behaviors.stopped
+      }
+    }
+}
